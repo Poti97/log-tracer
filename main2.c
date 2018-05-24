4f53d9e1e885bd63ad54e88038d245f199e652df
Ioshiro <fallenioshiro@gmail.com>
2018-05-25 00:34:53 +0200

    Add files via upload

--- main2.c
+++ main2.c
@@ -1,24 +1,42 @@
-#include <signal.h>
+#include<signal.h>
 #include <stdlib.h>
-#include <dtdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+
+
+
+time_t tstart,tend;
 
 void StartTime (int sig) {
-//inizializzi il tempo iniziale
-
+//inizializzo il tempo iniziale
+		tstart = time(NULL);
 
 }
 
 void EndTime () {
-//inizializzi il tempo di fine e stampi nel file le cose
-
-
+//inizializzo il tempo di fine e stampo nel file le cose
+	char* cttot, ctend;
+	tend = time(NULL);
+	ctend = ctime(&tend);
+	int fd[2];
+  char* time_spent_str = {"Time spent processing: "};
+	fd[0] = open("log.txt", O_WRONLY | O_CREAT, 0777);
+	write(fd[0], tstart, 32);
+	write(fd[0], ctend, 32);
+	write(fd[0], time_spent_str, strlen(time_spent_str));
+  close(fd[0]);
 	exit(0);
 }
 
 int main () {
 	pid_t pid;
-	pid = getpid()
+	pid = getpid();
 	signal(SIGUSR1, StartTime);
 	signal(SIGUSR2, EndTime);
 	while (1)
