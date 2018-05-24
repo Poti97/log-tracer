30acc37c63502969dc254714cccf3ef6ecfe68dc
Ioshiro <fallenioshiro@gmail.com>
2018-05-25 01:35:59 +0200

    Add files via upload

--- main2.c
+++ main2.c
@@ -21,15 +21,28 @@ void StartTime (int sig) {
 
 void EndTime () {
 //inizializzo il tempo di fine e stampo nel file le cose
-	char* cttot, ctend;
+	char* ctend;
+	char cttot[20];
+	double ttot = difftime(tend,tstart);
+//	snprintf(cttot, 20, "%f", ttot);
 	tend = time(NULL);
-	ctend = ctime(&tend);
+	char buf[20], buf2[20], buftot[20];
+	strftime(buf,20,"%Y-%m-%d %H:%M:%S",localtime(&tstart));
+	strftime(buf2,20,"%Y-%m-%d %H:%M:%S",localtime(&tend));
+	printf("buff: %s \n",buf);
 	int fd[2];
-  char* time_spent_str = {"Time spent processing: "};
+	char* start_str = "Start time = ";
+	char* end_str = "End time = ";
+  char* time_spent_str = "Time spent processing: ";
 	fd[0] = open("log.txt", O_WRONLY | O_CREAT, 0777);
-	write(fd[0], tstart, 32);
-	write(fd[0], ctend, 32);
+	write(fd[0], start_str, strlen(start_str));
+	write(fd[0], buf, strlen(buf));
+	write(fd[0], "\n", 1);
+	write(fd[0], end_str, strlen(end_str));
+	write(fd[0], buf2, strlen(buf2));
+	write(fd[0], "\n", 1);
 	write(fd[0], time_spent_str, strlen(time_spent_str));
+//	write(fd[0], ttot, strlen(ttot));  **QUESTO NON VA**
   close(fd[0]);
 	exit(0);
 }
@@ -38,8 +51,12 @@ int main () {
 	pid_t pid;
 	pid = getpid();
 	signal(SIGUSR1, StartTime);
+	kill(pid,SIGUSR1);
 	signal(SIGUSR2, EndTime);
-	while (1)
-		sleep(1);
-	return 0;
+	do {
+		sleep(3);
+	} while(0);
+	kill(pid,SIGUSR2);
+	
+	exit(EXIT_SUCCESS);
 }
